067108eb6df755c82d8e528de52aa59c08d2bdfa
benygg1337 <kozin@inetstudio.ru>
2023-08-30 01:26:46 +0300

    add_mail_template2

--- submit_wish.php
+++ submit_wish.php
@@ -52,21 +52,180 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST') {
             $mail->addAddress('dimon-951@mail.ru');
             $mail->addAddress('1337beny@gmail.com');
 
-            // Отправка сообщения
-            $mail->isHTML(true);
-            $mail->Subject = 'Пришло новое пожелание';
-            $mail->Body = 'К вам на сайт пришло новое пожелание:' . '<br>';
-            $mail->Body .= 'Имя отправителя: ' . $name . '<br>';
-            $mail->Body .= 'Текст пожелания: ' . $message . '<br> <br> Необходимо опубликовать пожелание!';
-
             // Формируем ссылки на подтверждение и удаление
             $theme_url = get_stylesheet_directory_uri();
             $confirmUrl = $theme_url . '/confirm-wish.php?wish_id=' . $post_id;
             $deleteUrl = $theme_url . '/delete-wish.php?wish_id=' . $post_id;
 
-            $mail->Body .= '<br><br>';
-            $mail->Body .= '<a href="' . $confirmUrl . '" style="background-color: #4CAF50; color: white; padding: 10px 20px; text-decoration: none; display: inline-block; margin-right: 10px;">Подтвердить пожелание</a>';
-            $mail->Body .= '<a href="' . $deleteUrl . '" style="background-color: #f44336; color: white; padding: 10px 20px; text-decoration: none; display: inline-block;">Удалить пожелание</a>';
+            $theme_uri = get_stylesheet_directory_uri();
+
+            // Вставляем HTML-шаблон письма здесь
+            $html_message = '
+            <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+            <html xmlns="http://www.w3.org/1999/xhtml">
+            <head>
+                <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+                <title>HTML Template</title>
+                <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+                <style>
+                    /* Ваш CSS стиль */
+                    body {
+                        width: 100% !important;
+                        -webkit-text-size-adjust: 100%;
+                        -ms-text-size-adjust: 100%;
+                        margin: 0;
+                        padding: 0;
+                        line-height: 100%;
+                    }
+                    [style*="Open Sans"] {font-family: \'Open Sans\', arial, sans-serif !important;}
+                    img {
+                        outline: none;
+                        text-decoration: none;
+                        border:none;
+                        -ms-interpolation-mode: bicubic;
+                        max-width: 100%!important;
+                        margin: 0;
+                        padding: 0;
+                        display: block;
+                    }
+                    table td {
+                        border-collapse: collapse;
+                    }
+                    table {
+                        border-collapse: collapse;
+                        mso-table-lspace: 0pt;
+                        mso-table-rspace: 0pt;
+                    }
+                    @media (max-width: 650px) {
+                      .table-650 {
+                        width: 280px !important;
+                      }
+                    }
+                </style>
+            </head>
+            <body style="margin: 0; padding: 0;">
+                <div style="font-size:0px;font-color:#ffffff;opacity:0;visibility:hidden;width:0;height:0;display:none;">
+                    Тестовое письмо
+                </div>
+                <table cellpadding="0" cellspacing="0" width="100%" bgcolor="#ededed">
+                    <tr>
+                        <td>
+                            <table align="center" class="table-700" cellpadding="0" cellspacing="0" width="700" bgcolor="#F2EEEB">
+                                <tr>
+                                    <td>
+                                        <table align="center" class="table-650" cellpadding="0" cellspacing="0" width="650">
+                                            <tr>
+                                                <td align="center" style="padding-top: 40px; padding-bottom: 40px;">
+                                                    <img src=/wp-content/themes/third/assets/merry-me.png" alt="Merry me <3">
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="padding-bottom: 100px; border-bottom: 1px solid #00000059;">
+                            <table align="center" class="table-700" cellpadding="0" cellspacing="0" width="700" bgcolor="#F2EEEB">
+                                <tr>
+                                    <td style="border: 1px solid #000000;">
+                                        <table align="center" class="table-650" cellpadding="0" cellspacing="0" width="650">
+                                            <tr>
+                                                <td align="center" style="padding-top: 25px; padding-bottom: 40px;">
+                                                    <img src=/wp-content/themes/third/assets/img-1.png" alt="main-img" width="650">
+                                                </td>
+                                            </tr>
+                                            <tr>
+                                                <td>
+                                                    <table align="center" class="table-620" cellpadding="0" cellspacing="0" width="620">
+                                                        <tr>
+                                                            <td>
+                                                                <p style="font-family: Verdana, Geneva, Tahoma, sans-serif; color: #000000; margin-top: 0; margin-bottom: 0; padding-bottom: 32px; font-size: 18px; line-height: 20px;">
+                                                                    К Вам на сайт пришло новое сообщение!
+                                                                </p>
+                                                                <p style="font-family: Verdana, Geneva, Tahoma, sans-serif; color: #000000; margin-top: 0; margin-bottom: 0; padding-bottom: 10px; font-size: 18px; line-height: 20px;">
+                                                                    Имя отправителя: ' . $name . '
+                                                                </p>
+                                                                <p style="font-family: Verdana, Geneva, Tahoma, sans-serif; color: #000000; margin-top: 0; margin-bottom: 0; padding-bottom: 32px; font-size: 18px; line-height: 20px;">
+                                                                    Текст пожелания: ' . $message . '
+                                                                </p>
+                                                                <p style="font-family: Verdana, Geneva, Tahoma, sans-serif; color: #000000; margin-top: 0; margin-bottom: 0; padding-bottom: 32px; font-size: 18px; line-height: 20px;">
+                                                                    Необходимо предпринять действие!
+                                                                </p>
+                                                            </td>
+                                                        </tr>
+                                                        <tr>
+                                                            <td align="center" style="padding-bottom: 32px;">
+                                                                <a href="' . $confirmUrl . '" style="display: inline-block; width: 270px; margin-right: 32px; padding-top: 12px; padding-bottom: 12px; border: 1px solid #03CE48; border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px; background-color: #03CE48; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #FFFFFF; font-size: 18px; line-height: 20px; text-align: center; text-decoration: none;">
+                                                                    Опубликовать пожелание
+                                                                </a>
+                                                                <a href="' . $deleteUrl . '" style="display: inline-block; width: 270px; padding-top: 12px; padding-bottom: 12px; border: 1px solid #C92222; border-top-right-radius: 10px; border-bottom-right-radius: 10px; border-top-left-radius: 10px; border-bottom-left-radius: 10px; background-color: #FF0707; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #FFFFFF; font-size: 18px; line-height: 20px; text-align: center; text-decoration: none;">
+                                                                    Удалить пожелание
+                                                                </a>
+                                                            </td>
+                                                        </tr>
+                                                    </table>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+
+                    <tr>
+                        <td style="padding-top: 50px; padding-bottom: 70px;">
+                            <table align="center" class="table-650" cellpadding="0" cellspacing="0" width="650" bgcolor="#F2EEEB">
+                                <tr>
+                                    <td>
+                                        <table align="center" class="table-600" cellpadding="0" cellspacing="0" width="600">
+                                            <tr>
+                                                <td align="center">
+                                                    <p style="display: block; width: 150px; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #00000070; font-size: 18px; line-height: 20px;">
+                                                        Мы в соцсетях
+                                                    </p>
+                                                    <a href="#" style="display: inline-block; margin-right: 20px;"> 
+                                                        <img src=/wp-content/themes/third/assets/vk.png" alt="vk">
+                                                    </a>
+                                                    <a href="#" style="display: inline-block;">
+                                                        <img src=/wp-content/themes/third/assets/telegram.png" alt="vk">
+                                                    </a>
+                                                </td>
+                                                <td align="center">
+                                                    <p style="display: block; width: 350px; font-family: Verdana, Geneva, Tahoma, sans-serif; color: #00000070; font-size: 18px; line-height: 20px;">
+                                                        Если у Вас есть вопросы, напишите нам удобным для Вас способом
+                                                    </p>
+                                                    <a href="#" style="display: inline-block; margin-right: 20px;">
+                                                        <img src=/wp-content/themes/third/assets/whatsapp.png" alt="whatsapp">
+                                                    </a>
+                                                    <a href="#" style="display: inline-block; margin-right: 20px;">
+                                                        <img src=/wp-content/themes/third/assets/vk.png" alt="vk">
+                                                    </a>
+                                                    <a href="#" style="display: inline-block; margin-right: 20px;">
+                                                        <img src=/wp-content/themes/third/assets/telegram.png" alt="telegram">
+                                                    </a>
+                                                    <a href="#" style="display: inline-block;">
+                                                        <img src=/wp-content/themes/third/assets/email.png" alt="email">
+                                                    </a>
+                                                </td>
+                                            </tr>
+                                        </table>
+                                    </td>
+                                </tr>
+                            </table>
+                        </td>
+                    </tr>
+                </table>
+            </body>
+            </html>';
+
+            // Теперь вставляем HTML-шаблон письма в тело письма
+            $mail->isHTML(true);
+            $mail->Subject = 'Пришло новое пожелание';
+            $mail->Body = $html_message;
 
             try {
                 $mail->send();
